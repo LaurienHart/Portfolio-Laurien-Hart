26e0146896604e97ad9e2e520e34df7f1dac627a
Laurien Hart <r0982944@student.thomasmore.be>
2025-08-15 11:19:01 +0200

    information page for Portfolio project added

---
+++ portfolio/src/app/projects/portfolio/page.tsx
@@ -0,0 +1,25 @@
+export default function portfolio() {
+    return (
+        <main className="max-w-5xl mx-auto py-20 px-6">
+            <h2 className="text-4xl font-bold mb-12 text-center">
+                Portfolio Website
+            </h2>
+
+            {/* Intro */}
+            <div className="mb-10">
+                <img
+                    src="/fotos/homeportfolio.png"
+                    alt="Home Portfolio"
+                    className="w-full rounded-2xl shadow-md object-cover mb-4"
+                />
+            </div>
+
+            {/* Back Link */}
+            <div className="text-center">
+                <a href="/projects" className="text-indigo-300 hover:underline font-medium">
+                    Terug
+                </a>
+            </div>
+        </main>
+    )
+}
