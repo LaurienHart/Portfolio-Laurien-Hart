0617221955d48393de34ce01e9afab979d618de8
Laurien Hart <r0982944@student.thomasmore.be>
2025-09-18 10:32:33 +0200

    Certificates added

--- portfolio/src/app/skills/page.tsx
+++ portfolio/src/app/skills/page.tsx
@@ -32,8 +32,13 @@ export default function Skills() {
         { name: "MS Office", icon: FileText },
     ]
 
+    const certificates = [
+        { name: "Snowflake", icon: Database, link: "/certificates/Snowflake%20certificate.pdf" },
+    ]
+
     return (
         <div className="max-w-4xl mx-auto py-20 px-6">
+            {/* Skills */}
             <h1 className="text-4xl font-bold mb-8 text-center">Skills</h1>
             <p className="text-gray-600 text-center mb-12">
                 Programmeertalen, frameworks en tools waarmee ik ervaring heb:
@@ -48,6 +53,23 @@ export default function Skills() {
                         <p className="text-lg font-semibold text-gray-800">{name}</p>
                         <Icon className="w-6 h-6 text-gray-500" />
                     </div>
+                ))}
+            </div>
+
+            {/* Certificaten */}
+            <h1 className="text-3xl font-bold my-12 text-center">Behaalde certificaten</h1>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {certificates.map(({ name, icon: Icon, link }, index) => (
+                    <a
+                        key={index}
+                        href={link}
+                        download
+                        className="p-6 rounded-2xl shadow-md bg-indigo-100 hover:shadow-lg transition flex items-center justify-between cursor-pointer"
+                    >
+                        <p className="text-lg font-semibold text-gray-800">{name}</p>
+                        <Icon className="w-6 h-6 text-gray-500" />
+                    </a>
                 ))}
             </div>
         </div>
